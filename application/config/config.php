1443374c1d53dd28559929827ee8bfd2b1f7dddd
Okta Pilopa <pilopaokta@gmail.com>
2025-09-23 11:43:58 +0800

    bug fixing

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/perkara/';
+$config['base_url'] = 'http://localhost/perkara_app/';
 
 
 /*
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/perkara/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -385,7 +385,7 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = null;
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
 $config['sess_match_ip'] = FALSE;
